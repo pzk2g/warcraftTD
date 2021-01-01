33c68b67fb9376c8343d56d67868fa5140982bf7
pzk2g <76259223+pzk2g@users.noreply.github.com>
2021-01-01 18:59:03 +0100

    Code récent

---
+++ src/warcraftTD/Bomb.java
@@ -0,0 +1,20 @@
+package warcraftTD.missiles;
+
+import warcraftTD.monsters.Monster;
+import warcraftTD.util.Position;
+
+public class Bomb extends Missile {
+	public static final String IMAGE = "images/Bomb.png";
+	public static final double SPEED = 0.02;
+	public static final int DAMAGE = 8;
+
+
+	/**
+	 * Classe qui gère les bombes
+	 * @param p la position du missile
+	 * @param target le monstre visé par la bombe
+	 */
+    public Bomb(Position p, Monster target){
+        super(p, target, SPEED, "images/Bomb.png", DAMAGE);
+    }
+}
