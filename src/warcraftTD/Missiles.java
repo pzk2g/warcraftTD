9e3c69d4122e9bc7de21b3f931ecf60fd6c3a9e7
pzk2g <76259223+pzk2g@users.noreply.github.com>
2021-01-01 18:56:11 +0100

    Code récent

--- src/warcraftTD/Missiles.java
+++ src/warcraftTD/Missiles.java
@@ -1,8 +1,45 @@
-package warcraftTD;
+package warcraftTD.missiles;
 
-public abstract class Missiles {
-	Position p;
-	String chemin;
-	Position nextP;
+import warcraftTD.monsters.Monster;
+import warcraftTD.util.ImageMobile;
+import warcraftTD.util.Position;
+import warcraftTD.util.StdDraw;
+
+
+public abstract class Missile extends ImageMobile{
+	//cible visée par le projectil
+	public Monster target;
+	//dommages créés par le projectile lorsqu'il atteint la cible
+	public int damage;
+	
+	
+	/**
+	 * Classe abstraite qui permet de gérer les projectiles
+	 * @param p la position du projectile
+	 * @param target le monstre visé par le projectile
+	 * @param speed la vitesse du projectile
+	 * @param image le chemin du projectile
+	 * @param damage les dommages infligée par le projectile
+	 */
+	public Missile(Position p, Monster target, double speed, String image, int damage) {
+		super(image, p, target.p, speed);
+		this.target = target;
+		this.damage = damage;
+	}
+
+	
+	@Override
+	public void draw(double normalizedX, double normalizedY){
+		double angle = super.calculateAngle(this.p, this.nextP);
+		StdDraw.picture(this.p.x, this.p.y, this.image, normalizedX, normalizedY, angle);
+	}
+	
+	
+	/**
+	 * Enlève des points de vies au monstre touché
+	 */
+	public void hit() {
+		this.target.takeLifePoint(damage);
+	}
 	
 }
