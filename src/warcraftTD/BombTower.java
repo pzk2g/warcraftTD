8f59b8f42285cf846d89263f54d660c9062fa185
pzk2g <76259223+pzk2g@users.noreply.github.com>
2021-01-01 18:24:38 +0100

    Code récent

--- src/warcraftTD/BombTower.java
+++ src/warcraftTD/BombTower.java
@@ -1,7 +1,46 @@
-package warcraftTD;
+package warcraftTD.towers;
+
+import warcraftTD.missiles.Bomb;
+import warcraftTD.missiles.Missile;
+import warcraftTD.monsters.BaseMonster;
+import warcraftTD.monsters.Boss;
+import warcraftTD.monsters.Monster;
+import warcraftTD.util.Position;
 
 public class BombTower extends Tower{
+
+	public static final String IMAGE = "images/BombTowerLevel1.png";
+	public static final int SPEED = 20;
+	public static final int PRICE = 60;
+	public static final double  REACH = 0.15;
+
+	/**
+	 * Classe des tours de Bombes
+	 * @param p la position de la tour
+	 */
 	public BombTower(Position p) {
-		super(p, "images/BombTower.png", 20, 60, 0.2);
+		super(p,IMAGE, SPEED, PRICE, REACH);
+	}
+	
+	@Override
+	public Missile attack(Monster m) {
+		long tps = System.currentTimeMillis();
+		if (tps-this.time>this.speed) {
+			if (m instanceof BaseMonster || 
+			((m instanceof Boss) && ((Boss)m).state=='w'))
+				if (m.p.dist(this.p) <= this.reach) {
+					this.time = tps;
+					return new Bomb(this.p.clone(), m);
+				}
+		}
+		return null;
+	}
+	
+	@Override
+	public void updating() {
+		this.level++;
+		this.image = String.format("images/BombTowerLevel%d.png", this.level);
+		this.speed -= (SPEED/2)*10;
+		this.reach += REACH/2;
 	}
 }
