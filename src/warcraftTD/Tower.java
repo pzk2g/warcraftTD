f423c7d2bef7c5f57f859d0441f38f08d4048a15
pzk2g <76259223+pzk2g@users.noreply.github.com>
2021-01-01 18:22:02 +0100

    Code récent

--- src/warcraftTD/Tower.java
+++ src/warcraftTD/Tower.java
@@ -1,40 +1,77 @@
-package warcraftTD;
+package warcraftTD.towers;
+
+import warcraftTD.missiles.Missile;
+import warcraftTD.monsters.Monster;
+import warcraftTD.util.Position;
+import warcraftTD.util.StdDraw;
 
 public abstract class Tower {
 	//position du tour à l'instant t
-	Position p;
+	public Position p;
 	//vitesse d'attaque de la tour
-	int speed;
+	public int speed;
 	//portée de l'attaque de la tour
-	double reach;
+	public double reach;
 	//prix de contruction d'une tour
-	int price;
+	public int price;
 	//vitesse des projectiles
-	double speedMissile;
+	public double speedMissile;
 	//chemins des tours à afficher
-	String chemin;
-	
+	String image;
+	//temps de rechargement des projectiles
+	protected long time;
+	//niveau de la tour
+	public int level;
+	//prix amélioration d'une tour
+	public final static int UPDATEPRICE = 30;
+
+	/**
+	 * Classe abstraite qui permet de gérer les différentes types de tours 
+	 * @param p la Position de la tour
+	 * @param chemin l'url de l'image de la tour
+	 * @param speed la vitesse de rechargement de la tour
+	 * @param price le prix de production de la tour
+	 * @param reach la distance de visée de la tour
+	 */
 	public Tower(Position p, String chemin, int speed, int price, double reach) {
 		this.p = p;
-		this.speed = speed;
+		this.speed = speed*10;
 		this.price = price;
-		this.chemin = chemin;
+		this.image = chemin;
 		this.reach = reach;
+		this.time = System.currentTimeMillis();
+		this.level = 1;
 	}
 	
+	/**
+	 * Dessine une image de la tour dans le cadrillage
+	 * @param normalizedX la taille en largeur d'un carré du cadrillage
+	 * @param normalizedY la taille en longeur d'un carré du cadrillage
+	 */
 	public void update(double normalizedX, double normalizedY) {
 		draw(normalizedX, normalizedY);
 	}
 	
 	/**
-	 * Lance un missile en direction du monster à la position p
-	 * @param p la position du monstre à attaquer
+	 * Lance un projectile en direction du monster m
+	 * @param m le monstre pris en cible
 	 */
-	protected void attack(Position p, String chemin) {
-		StdDraw.picture(p.x, p.y, chemin);
-	}
+	public abstract Missile attack(Monster m);
 	
 	public void draw(double normalizedX, double normalizedY) {
-		StdDraw.picture(p.x, p.y, chemin, normalizedX, normalizedY);
+		StdDraw.picture(p.x, p.y, image, normalizedX, normalizedY);
 	}
+	
+	/**
+	 * Indique si la tour est améliorable ou non
+	 * @return vrai si la tour est améliorable
+	 */
+	public boolean isUpdatable() {
+		return level<3;
+	}
+	
+	/**
+	 * Augmente d'un niveau le niveau de la tour
+	 */
+	public abstract void updating();
 }
