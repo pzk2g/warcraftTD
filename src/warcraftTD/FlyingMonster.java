92eed6e8bdc880931938015e3da20852dac6a415
pzk2g <76259223+pzk2g@users.noreply.github.com>
2021-01-01 18:09:51 +0100

    code récent

--- src/warcraftTD/FlyingMonster.java
+++ src/warcraftTD/FlyingMonster.java
@@ -1,7 +1,29 @@
-package warcraftTD;
+package warcraftTD.monsters;
 
+import warcraftTD.util.Position;
+
+//TODO : creer un boss
 public class FlyingMonster extends Monster {
-	public FlyingMonster(Position p) {
-		super(p, "images/FlyingMonster.png", 3, 0.02, 8);
+	public static final int LIFE = 3;
+	public static final int REWARD = 8;
+	public static final double SPEED = 0.01;
+	public static final String IMAGE = "images/FlyingMonster.png";
+	
+	//TODO : ajouter les monstres plus résistants : des niveaux différents pour plus de difficulté
+	//avec notamment des images différentes
+	
+	
+	/**
+	 * Classe qui gère les monstres volants
+	 * @param p
+	 */
+	public FlyingMonster(Position p, int level) {
+		super(IMAGE, p, LIFE, SPEED, REWARD, level);
+		System.out.println(this.life);
+	}
+
+	@Override
+	public void takeLifePoint(int damage){
+		takeLifePoint(LIFE, damage);
 	}
 }
