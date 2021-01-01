63e0490304c8e698263228b68a771ed53463195f
pzk2g <76259223+pzk2g@users.noreply.github.com>
2021-01-01 18:07:16 +0100

    Update BaseMonster.java
    code récent

--- src/warcraftTD/BaseMonster.java
+++ src/warcraftTD/BaseMonster.java
@@ -1,8 +1,23 @@
-package warcraftTD;
+package warcraftTD.monsters;
 
+//TODO : creer un boss (une classe fille)
 public class BaseMonster extends Monster {
-	int niveau; //niveau du montre
-	public BaseMonster(Position p) {
-		super(p, "images/BaseMonster.png", 5, 0.01, 5);
+	public static final int LIFE = 5;
+	public static final int REWARD = 5;
+	public static final double SPEED = 0.005;
+	public static final String IMAGE = "images/BaseMonster.png";
+	
+	/**
+	 * Classe qui gère les monstres qui marchent
+	 * @param p une position
+	 * @param level un entier compris entre 1 et 4
+	 */
+	public BaseMonster(Position p, int level) {
+		super(IMAGE, p, LIFE, SPEED, REWARD, level);
+	}
+
+	@Override
+	public void takeLifePoint(int damage){
+		takeLifePoint(LIFE, damage);
 	}
 }
