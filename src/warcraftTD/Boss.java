c61fa24fe1451e9e5d689d0a1db31fac39b150f5
pzk2g <76259223+pzk2g@users.noreply.github.com>
2021-01-08 23:55:55 +0100

    Update Boss.java

--- src/warcraftTD/Boss.java
+++ src/warcraftTD/Boss.java
@@ -1,71 +1,91 @@
-package warcraftTD.monsters;
-public class Boss extends Monster {
-    
-    public  int life;
-    public  int reward;
-    public  double speed;
-    public static final String IMAGE = "images/Boss.png";
-    protected long time;
-    public char state;
+package warcraftTD;
 
-    public Boss(Position p, int level) {
-        super(IMAGE, p, setLife(level), setSpeed(level), setReward(level), level);
-    }
-   	private static int setReward(int level) {
-		int reward = 0;
-		switch(level) {
-		case 1:
-			reward = 60;
-			break;
-		case 2:
-			reward = 110;
-			break;
-		case 3:
-			reward = 160;
-			break;
-		}
-		return reward;
+public class Boss extends Monster {
+	public static final String IMAGEFLY = "images/Boss.png";
+	public static final String IMAGEWALK = "images/BossInFire.png";
+	public static final long TRANSFORMTIME = 15000; // Le monstre se transforme au bout de 20 secondes
+	public static String image = IMAGEFLY;
+
+	public char state;
+
+	public Boss(Position p, int level) {
+		super(image, p, level > 3 ? 3 : level);
+		this.time = System.currentTimeMillis();
 	}
 
-	private static double setSpeed(int level) {
-		double speed = 0;
-		switch(level) {
-		case 1:
-			speed = 0.02;
-			break;
-		case 2:
-			speed = 0.05;
-			break;
-		case 3:
-			speed = 0.08;
-			break;
-		}
+	/**
+	 * Augmente la recompense donnée par le boss éliminé en fonction de son niveau
+	 * dans une partie
+	 * 
+	 * @param level
+	 */
+	@Override
+	protected void setReward(int level) {
+		this.reward = 0;
+		for (int i = 1; i <= level; i++)
+			this.reward += 60;
+	}
+
+	/**
+	 * Augmente la vitesse du boss en fonction de son niveau dans une partie
+	 * 
+	 * @param level
+	 */
+	@Override
+	protected double setSpeed(int level) {
+		double speed = 0.005;
+		for (int i = 1; i < level; i++)
+			speed += 0.003;
 		return speed;
 	}
-	
-	public static int setLife(int level) {
-		int life = 0;
-		switch(level) {
+
+	/**
+	 * Augmente le nombre de point de vie du boss en fonction de son niveau dans une
+	 * partie
+	 * 
+	 * @param level
+	 */
+	@Override
+	protected void setLife(int level) {
+		switch (level) {
 		case 1:
-			life = 15;
+			life = 100;
 			break;
 		case 2:
-			life = 25;
+			life = 175;
 			break;
 		case 3:
-			life = 50;
+			life = 250;
 			break;
+		default:
+			throw new IllegalArgumentException("Level must be between 1 et 3");
 		}
-		return life;	
-    }
-    
-    @Override
-    public void takeLifePoint(int damage) {
-        takeLifePoint(life, damage);
-    }
+	}
 
-    public char getState(){
-        //TODO : à finir
-        return ' ';
-    }
+	/**
+	 * Transformation du Boss de monstre volant à monstre de base au cours du jeu
+	 */
+	protected String transform() {
+		long t = System.currentTimeMillis();
+		if (image.equalsIgnoreCase(IMAGEFLY)) {
+			if (t - this.time > TRANSFORMTIME) {
+				this.state = 'w';
+				image = IMAGEWALK;
+				this.time = t;
+			}
+		}
+		return image;
+	}
+
+	@Override
+	public void draw(double normalizedX, double normalizedY) {
+		StdDraw.picture(getP().getX(), getP().getY(), transform(), normalizedX, normalizedY);
+		long t = System.currentTimeMillis();
+		if (t - this.time > TRANSFORMTIME) {
+			image = IMAGEFLY;
+			this.state = 'f';
+			this.time = t;
+		}
+	}
+
 }
